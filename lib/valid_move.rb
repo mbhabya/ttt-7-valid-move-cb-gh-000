5bee7667c6033fa19cbb5b0d4c385dcedec6eda0
Bhabya  Mishra <mbhabya@gmail.com>
2017-09-17 07:55:21 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,6 +2,7 @@
 def valid_move?(board,index)
 
 return false if((board[index]!= 1)||(board[index]!= 2)||(board[index]!= 3)||(board[index]!= 4)||(board[index]!= 5)||(board[index]!= 6)||(board[index]!= 7)||(board[index]!= 8)||(board[index]!= 9)
+return false if (position_taken?(board,index) == false)
 else
   true
 end
